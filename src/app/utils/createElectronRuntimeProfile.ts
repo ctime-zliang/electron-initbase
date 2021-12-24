4f6dfbb29943bc2962170bcff424ad57632b40bc
richardadda <1156282805@qq.com>
2021-12-25 02:42:59 +0800

    feat: modify - daily commit

--- src/app/utils/createElectronRuntimeProfile.ts
+++ src/app/utils/createElectronRuntimeProfile.ts
@@ -1,10 +1,5 @@
 import { IGlobalWindowItemProfile } from '@utypes/electron.types'
 
 export const createElectronAppRuntimeProfile = (option: IGlobalWindowItemProfile): IGlobalWindowItemProfile => {
-	return Object.create({
-		id: option.id,
-		win: option.win,
-		pageInitURL: option.pageInitURL,
-		windowBounds: option.windowBounds,
-	})
+	return Object.create({ ...option })
 }
